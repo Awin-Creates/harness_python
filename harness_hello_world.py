525bbcbe8404c90aad0e0edd0686f29d67e1f8c0
Awin-Creates <66404147+Awin-Creates@users.noreply.github.com>
2024-02-01 03:14:48 +0530

    updating random print

--- harness_hello_world.py
+++ harness_hello_world.py
@@ -1,2 +1,13 @@
 print("Hello world!")
 print("Using this as my first Harness Deployment")
+
+
+import random
+
+def main():
+    print("Hello World from Python")
+    return random.randint(100, 200)
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
